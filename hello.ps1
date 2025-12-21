473a6cb22b46a9d895fd5d49cf4819df060402bf
ludolpif <ludolpif@gmail.com>
2025-12-21 13:08:04 +0100

    CI: may related to CRLF ?

--- hello.ps1
+++ hello.ps1
@@ -1,1 +1,11 @@
+param (
+    [Parameter(Mandatory = $false)]
+    [string]$Configuration
+)
+
+if ($Configuration -ne "Debug" -and $Configuration -ne "Release") {
+    [Console]::Error.WriteLine("Usage: $PSCommandPath (Debug|Release)")
+    exit 1
+}
+
 Write-Host "Hello World"
