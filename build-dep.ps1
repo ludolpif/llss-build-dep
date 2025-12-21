f87f1fa636c036d4c98d3e90b0066a3a70742a8e
ludolpif <ludolpif@gmail.com>
2025-12-21 10:48:53 +0100

    CI: fixes

--- build-dep.ps1
+++ build-dep.ps1
@@ -2,7 +2,7 @@ $version = "3.2.10"
 $url = "https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL3-devel-${version}-VC.zip"
 
 Set-PSDebug -Strict -Step
-Push-Location "lib\platform"
+cd "lib\platform"
 Remove-Item -Recurse -Force -Path "sdl3-devel-vc" -ErrorAction SilentlyContinue
 if (-Not (Test-Path "sdl3-devel-vc.zip")) {
     Invoke-WebRequest -Uri $url -OutFile "sdl3-devel-vc.zip"
@@ -11,4 +11,3 @@ Expand-Archive -Path "sdl3-devel-vc.zip" -DestinationPath "." -Force
 Get-ChildItem -Force # ls -a equivalent
 Rename-Item -Path "SDL3-${version}" -NewName "sdl3-devel-vc"
 Remove-Item -Path "sdl3-devel-vc.zip"
-Pop-Location
