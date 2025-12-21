092ff95ba8f2650a03498a65f5cb1f4caeafb687
ludolpif <ludolpif@gmail.com>
2025-12-21 12:15:53 +0100

    CI: fixes

--- build-dep.ps1
+++ build-dep.ps1
@@ -1,7 +1,16 @@
-$version = "3.2.10"
-$url = "https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL3-devel-${version}-VC.zip"
+param (
+    [Parameter(Mandatory = $false)]
+    [string]$Configuration
+)
+
+if ($Configuration -ne "Debug" -and $Configuration -ne "Release") {
+    [Console]::Error.WriteLine("Usage: $PSCommandPath (Debug|Release)")
+    exit 1
+}
 
 Set-PSDebug -Strict -Step
+$version = "3.2.10"
+$url = "https://github.com/libsdl-org/SDL/releases/download/release-${version}/SDL3-devel-${version}-VC.zip"
 
 Get-Location
 ls
