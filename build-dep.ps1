623c2373aa8df31dbc555fd26a52e82aa42a9a82
ludolpif <ludolpif@gmail.com>
2025-12-21 23:22:18 +0100

    build: tidy build files, keep in sync with llss project

--- build-dep.ps1
+++ build-dep.ps1
@@ -31,42 +31,42 @@ $build_dep_version = "0.2.0.0"
 $build_dep_zip = "build-dep-${build_dep_version}-Sources.zip"
 $build_dep_url = "https://ludolpif.fr/pub/llss/artifacts/$build_dep_zip"
 
-'::notice:: Push-Location "lib\platform"'
+"::notice:: Push-Location "lib\platform""
 Push-Location "lib\platform"
 
-'::notice:: remove sdl3-devel-vc folder if any'
+"::notice:: remove sdl3-devel-vc folder if any"
 Remove-Item -Recurse -Force -Path "sdl3-devel-vc" -ErrorAction SilentlyContinue
 
-'::notice:: re-use or download "sdl3-devel-vc.zip'
+"::notice:: re-use or download 'sdl3-devel-vc.zip'"
 if (-Not (Test-Path "sdl3-devel-vc.zip")) {
     Invoke-WebRequest -Uri $sdl_url -OutFile "sdl3-devel-vc.zip"
 }
-'::notice:: unzip "sdl3-devel-vc.zip"'
+"::notice:: unzip 'sdl3-devel-vc.zip'"
 Expand-Archive -Path "sdl3-devel-vc.zip" -DestinationPath "." -Force
 
-'::group::lib\platform content before renaming'
+"::group::lib\platform content before renaming"
 Get-ChildItem -Force # ls -a equivalent
-'::endgroup::'
+"::endgroup::"
 
 Rename-Item -Path "SDL3-${sdl_version}" -NewName "sdl3-devel-vc"
 
 # Remove-Item -Path "sdl3-devel-vc.zip"
-'::group::lib\platform content at end of script'
+"::group::lib\platform content at end of script"
 Get-ChildItem -Force # ls -a equivalent
-'::endgroup::'
+"::endgroup::"
 
 '::notice:: Pop-Location to git root'
 Pop-Location
 
 
-'::notice:: re-use or download "$build_dep_zip"'
+"::notice:: re-use or download '$build_dep_zip'"
 if (-Not (Test-Path "$build_dep_zip")) {
     Invoke-WebRequest -Uri $build_dep_url -OutFile "$build_dep_zip"
 }
-'::notice:: unzip "$build_dep_zip"'
+"::notice:: unzip '$build_dep_zip'"
 Expand-Archive -Path "$build_dep_zip" -DestinationPath "." -Force
 
 # Remove-Item -Path "$build_dep_zip"
-'::group::lib\platform content at end of script'
+"::group::lib\platform content at end of script"
 Get-ChildItem -Force # ls -a equivalent
-'::endgroup::'
+"::endgroup::"
